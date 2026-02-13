1faffbe755e296812f735e3db79f0bbf3b21c3a8
Shivaditya S S <shivaditya2503@gmail.com>
2026-02-13 23:22:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hello")
+console.log("I like pizza")
+
+//window.alert("Die sooner")
+document.getElementById("h1").textContent="Hello";
+document.getElementById("p1").textContent="Please die sooner";
